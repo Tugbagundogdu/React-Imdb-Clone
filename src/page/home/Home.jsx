1e7a836063c2d445afd95f7d77e06c40381c0a71
Tugbagundogdu <78304413+Tugbagundogdu@users.noreply.github.com>
2023-05-11 23:43:43 +0300

    axios

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,10 +1,10 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import {Link} from 'react-router-dom' 
 import './home.css'
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
+import axios from 'axios';
 
 
 const Home = () => {
